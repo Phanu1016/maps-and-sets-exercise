2d351f3ba141b2f95d7943bd71bd3a0192317d37
Phanu <admin@phanuphanu.com>
2022-07-09 13:40:32 -0700

    vowelCount()

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-const hasDuplicate = (arr) => arr.length === [...new Set(arr)].length ? false : true
+function vowelCount(str){
+    const vowelMap = new Map()
+    for (let char of str.toLowerCase()){
+        "aeiou".indexOf(char) >= 0 ? (vowelMap.has(char) ? vowelMap.set(char, vowelMap.get(char)+1) : vowelMap.set(char, 1)) : undefined
+    }
+    return vowelMap
+}
