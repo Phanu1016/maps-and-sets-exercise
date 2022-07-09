e36cd84718ec84ccf27971851462c232857c79c3
Phanu <admin@phanuphanu.com>
2022-07-09 13:30:07 -0700

    hasDuplicate()

--- app.js
+++ app.js
@@ -1,10 +1,1 @@
-// Quick Question #1
-new Set([1,1,2,2,3,4]) // {1,2,3,4}
-
-// Quick Question #2
-[...new Set("referee")].join("") // "ref"
-
-// Quick Question #3
-let m = new Map();
-m.set([1,2,3], true); // 0: {Array(3) => true}
-m.set([1,2,3], false); // 1: {Array(3) => false}
+const hasDuplicate = (arr) => arr.length === [...new Set(arr)].length ? false : true
